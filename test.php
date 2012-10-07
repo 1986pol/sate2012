0bbe6f5d32c032eccaad3f08c48522a8982b7c15
pol <plev@i.ua>
2012-10-07 20:59:36 +0300

    add book message

--- test.php
+++ test.php
@@ -1,6 +1,12 @@
 <?
-$q='не знаю как правльно вывести текст';
-$w=strlen($q);
-echo"$w";
-#pol
+$sdd_db_host='localhost';// ваш адрес где находится, хостится ваша база данных
+$sdd_db_name='test';// Имя базы данных с которой вы хотите работать, так как их может быть множество
+$sdd_db_user='root';// логин доступ к базе данных
+$sdd_db_pass='7766421';// пароль доступа к базе данных
+@mysql_connect($sdd_db_host,$sdd_db_user,$sdd_db_pass);// устанавливаем связь с сервером
+@mysql_select_db($sdd_db_name);// переключаемся на нужную нам базу данных
+$q=mysql_query('SELECT * FROM `messages`');// делаем выборку из таблицы
+while($row=mysql_fetch_array($q))// берем результаты из каждой строки
+	echo '<b>'.$row['title'].'</b><br><b>'.$row['message'].'</b><br>'.$row['nik'].'...'.$row['data'].'<hr>';
+	
 ?>
